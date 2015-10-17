4a1966dd60132dfc29b95bbaa8d210177a64972d
John Otander <johnotander@gmail.com>
2015-10-17 11:24:08 -0600

    Update css-url-regex

--- test.js
+++ test.js
@@ -2,13 +2,17 @@ import test from 'ava'
 import isBlank from 'is-blank'
 import getCssUrls from './'
 
-test(t => {
-  t.plan(2)
+test('gets css urls', t => {
+  t.plan(1)
 
   t.same(
     getCssUrls("url(foo.css) lksjhlksjhdf url(bar.css)"),
     ["url(foo.css)", "url(bar.css)"]
   )
+})
+
+test('does not get nonexistent urls', t => {
+  t.plan(1)
 
   t.ok(isBlank(getCssUrls('(foo.css) lksjhlksjhdf')))
 })
